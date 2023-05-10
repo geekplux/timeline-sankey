67b44978aeeb2850f0a7b7d8fcdc6efc941ea3fd
geekplux <geekplux@gmail.com>
2023-05-09 23:24:30 -0400

    feat: minor change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,26 +22,14 @@ export default function TimelinePage() {
       step: 5
     },
     yearHeight: {
-      value: 110, // height of per year
+      value: 60, // height of per year
       min: MIN_YEAR_HEIGHT,
       max: MAX_YEAR_HEIGHT,
       step: 10
     },
-    mobileYearHeight: {
-      value: 50, // height of per year
-      min: 20,
-      max: 100,
-      step: 5
-    },
     nodeWidth: {
       value: 12,
       min: 5,
-      max: 30,
-      step: 1
-    },
-    mobileNodeWidth: {
-      value: 8,
-      min: 3,
       max: 30,
       step: 1
     },
@@ -68,14 +56,26 @@ export default function TimelinePage() {
       value: 1,
       min: 1,
       max: 20,
-      step: 1,
+      step: 1
     },
     crossLinkTriangleSize: {
       value: 7,
       min: 5,
       max: 20,
-      step: 1,
+      step: 1
     },
+    mobileYearHeight: {
+      value: 50, // height of per year
+      min: 20,
+      max: 100,
+      step: 5
+    },
+    mobileNodeWidth: {
+      value: 8,
+      min: 3,
+      max: 30,
+      step: 1
+    }
   });
 
   const {
@@ -93,36 +93,42 @@ export default function TimelinePage() {
     influencedByMaxCount,
     crossInfluencedMaxCount,
     crossInfluencedByMaxCount
-  } = useControls('Random Data Generation Parameters', {
-    nodeCount: {
-      value: 40,
-      min: 10,
-      max: 100,
-      step: 10,
+  } = useControls(
+    'Random Data Generation Parameters',
+    {
+      nodeCount: {
+        value: 40,
+        min: 10,
+        max: 100,
+        step: 10
+      },
+      yearRange: {
+        min: 1600,
+        max: 2022,
+        value: [1800, 2022]
+      },
+      columnCount: {
+        value: 3,
+        min: 2,
+        max: 5,
+        step: 1
+      },
+      tagMaxCount: 10,
+      categoryMaxCount: 10,
+      color1: { value: 'peru', render: (get) => get('columnCount') >= 1 },
+      color2: { value: 'lightpink', render: (get) => get('columnCount') >= 2 },
+      color3: { value: 'steelblue', render: (get) => get('columnCount') >= 3 },
+      color4: { value: 'red', render: (get) => get('columnCount') >= 4 },
+      color5: { value: 'limegreen', render: (get) => get('columnCount') >= 5 },
+      influencedMaxCount: 3,
+      influencedByMaxCount: 3,
+      crossInfluencedMaxCount: 6,
+      crossInfluencedByMaxCount: 6
     },
-    yearRange: {
-      min: 1600,
-      max: 2022,
-      value: [1800, 2022]
-    },
-    columnCount: {
-      value: 3,
-      min: 2,
-      max: 5,
-      step: 1
-    },
-    tagMaxCount: 10,
-    categoryMaxCount: 10,
-    color1: { value: 'peru', render: (get) => get('columnCount') >= 1 },
-    color2: { value: 'lightpink', render: (get) => get('columnCount') >= 2 },
-    color3: { value: 'steelblue', render: (get) => get('columnCount') >= 3 },
-    color4: { value: 'red', render: (get) => get('columnCount') >= 4 },
-    color5: { value: 'limegreen', render: (get) => get('columnCount') >= 5 },
-    influencedMaxCount: 3,
-    influencedByMaxCount: 3,
-    crossInfluencedMaxCount: 6,
-    crossInfluencedByMaxCount: 6
-  });
+    {
+      collapsed: true
+    }
+  );
 
   useEffect(() => {
     setData(
